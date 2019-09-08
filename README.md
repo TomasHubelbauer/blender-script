bab1e901488f83a8cc0ec2e7c9dbef6479e384f9
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-09-08 22:00:48 +0200

    Update the demo text color

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ![](render.png)
 
-1. Add Blender to `%PATH%`
+1. Add Blender to `%PATH%` and run `pip install bpy`
 2. Open the repository in VS Code
 3. Open `script.py` and `render.png` in split view side by side
 3. Open the VS Code terminal
@@ -14,3 +14,7 @@
   - Removing `--background`
   - Splitting terminal and running `blender --python script.py`
   - Pressing F12 in Blender to preview render
+
+## To-Do
+
+Figure out how to get autocompletion for `bpy`.
--- script.py
+++ script.py
@@ -1,12 +1,22 @@
 import os
 import bpy
+import math
 
 # Add another cude aside from the default cube
 bpy.ops.mesh.primitive_cube_add(location=(1, 1, 1))
 
 # Add a text to the scene
-bpy.ops.object.text_add(location=(0, -.75, 1.5))
-bpy.context.object.data.body = "Blender"
+bpy.ops.object.text_add(
+    location=(-.5, -1.75, 1),
+    rotation=(math.pi / 2, 0, math.pi / 4)
+)
+
+bpy.context.object.data.body = "Hello     hi :)"
+
+# Make the text red
+red = bpy.data.materials.new("Red")
+red.diffuse_color = (1, 0, 0, 0)  # RGB?
+bpy.context.object.data.materials.append(red)
 
 # Direct the render to the repository directory
 bpy.data.scenes['Scene'].render.filepath = os.getcwd() + '/render.png'
